f52e88db35c28e6829e63ea57b1a049e2a31703c
Yordan Stoev <sularome@gmail.com>
2020-03-23 21:25:34 +0100

    fix: add cors * since git action is triggered from unknown ip

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("./utils/loadEnv").load();
 
 async function App() {
     const app = express();
-    setupCors(app);
+    app.use(cors());
     await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -15,7 +15,7 @@ async function App() {
     app.listen(process.env.PORT, () => {
         console.log("Server running on port 3000");
     });
-    app.get("/total", (req, res, next) => {
+    app.get("/total", (req, res) => {
         Models.DailyStatistics.aggregate([{
             $unwind: "$regions"
         }, {
@@ -33,25 +33,6 @@ async function App() {
         });
     });
     return app;
-}
-
-function setupCors(app) {
-    const allowedOrigins = [`http://localhost:${process.env.PORT}`,
-        `https://sularome.github.io/koronawirus`];
-    app.use(cors({
-        origin: function (origin, callback) {    // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) {
-                return callback(null, true);
-            }
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg = 'The CORS policy for this site does not ' +
-                    'allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        }
-    }));
 }
 
 App();
