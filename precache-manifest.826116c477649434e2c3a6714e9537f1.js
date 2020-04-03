f9fbd9a4af63821acccebaf7a124308d1869b157
sularome <sularome@users.noreply.github.com>
2020-04-03 21:30:40 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.826116c477649434e2c3a6714e9537f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced0c0495146993197a04109f675077d",
+    "url": "/koronawirus/index.html"
+  },
+  {
+    "revision": "a2b6edd616b72820c4e0",
+    "url": "/koronawirus/static/css/main.15810bff.chunk.css"
+  },
+  {
+    "revision": "54aaa4df781571c17511",
+    "url": "/koronawirus/static/js/2.38dcb6b6.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/koronawirus/static/js/2.38dcb6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b6edd616b72820c4e0",
+    "url": "/koronawirus/static/js/main.a161e183.chunk.js"
+  },
+  {
+    "revision": "ae57dc9a3df72c859960",
+    "url": "/koronawirus/static/js/runtime-main.1bf8998a.js"
+  }
+]);
